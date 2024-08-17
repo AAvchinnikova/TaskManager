b0b83adbd4b7ac9793f296539a41064220b178fd
AAvchinnikova <avchinnikova_a@mail.ru>
2024-08-17 11:01:43 +0200

    step 1

--- src/main/java/hexlet/code/conroller/WelcomeController.java
+++ src/main/java/hexlet/code/conroller/WelcomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-    @GetMapping(path = "")
+    @GetMapping
     public String welcome() {
         return "Welcome to Spring";
     }

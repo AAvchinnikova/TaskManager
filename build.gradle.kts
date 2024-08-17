b6f9cbed3ad6a67b841c14a576c0de763c63ab42
AAvchinnikova <avchinnikova_a@mail.ru>
2024-08-17 11:21:22 +0200

    step 1

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
 	id("org.springframework.boot") version "3.3.3-SNAPSHOT"
 	id("io.spring.dependency-management") version "1.1.6"
 	id("com.github.ben-manes.versions") version "0.50.0"
+	id("io.freefair.lombok") version "8.6"
 
 }
 

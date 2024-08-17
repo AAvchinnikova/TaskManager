9d7cba3e8db33d83ae6255e9f0d0a4a6d4366498
AAvchinnikova <avchinnikova_a@mail.ru>
2024-08-17 11:13:58 +0200

    step 1

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 	id("org.springframework.boot") version "3.3.3-SNAPSHOT"
 	id("io.spring.dependency-management") version "1.1.6"
 	id("com.github.ben-manes.versions") version "0.50.0"
-	id("io.freefair.lombok") version "8.6"
+
 }
 
 group = "hexlet.code"
@@ -29,6 +29,8 @@ repositories {
 }
 
 dependencies {
+	compileOnly("org.projectlombok:lombok:1.18.30")
+	annotationProcessor("org.projectlombok:lombok:1.18.30")
 
 	implementation("org.mapstruct:mapstruct:1.5.5.Final")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
@@ -50,6 +52,8 @@ dependencies {
 	testImplementation(platform("org.junit:junit-bom:5.10.0"))
 	testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
 	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+	testCompileOnly("org.projectlombok:lombok:1.18.30")
+	testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 

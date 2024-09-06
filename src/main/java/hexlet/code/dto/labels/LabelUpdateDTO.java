8e13f6ef519abcdf54728db44d371b12c409fdeb
AAvchinnikova <avchinnikova_a@mail.ru>
2024-09-06 09:44:08 +0200

    step 10

--- src/main/java/hexlet/code/dto/labels/LabelUpdateDTO.java
+++ src/main/java/hexlet/code/dto/labels/LabelUpdateDTO.java
@@ -1,6 +1,5 @@
 package hexlet.code.dto.labels;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
--- src/main/java/hexlet/code/dto/task/statuses/TaskStatusCreateDTO.java
+++ src/main/java/hexlet/code/dto/task/statuses/TaskStatusCreateDTO.java
@@ -1,6 +1,5 @@
 package hexlet.code.dto.task.statuses;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
